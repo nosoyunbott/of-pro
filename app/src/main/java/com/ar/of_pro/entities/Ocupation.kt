74f24e29759cd17c8da140c2fec7273b54a0c62c
felor <felodrobles@gmail.com>
2023-09-17 20:04:55 -0300

    spinner ocupations done

---
+++ app/src/main/java/com/ar/of_pro/entities/Ocupation.kt
@@ -0,0 +1,10 @@
+package com.ar.of_pro.entities
+
+class Ocupation {
+
+    val ocupationList = listOf("Plomeria", "Pintura", "Carpinteria", "Gasista", "Electricista")
+
+    public fun getList(): List<String> {
+        return ocupationList
+    }
+}
--- app/src/main/java/com/ar/of_pro/fragments/RequestFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/RequestFragment.kt
@@ -1,22 +1,77 @@
 package com.ar.of_pro.fragments
 
+import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.ar.of_pro.R
+import com.ar.of_pro.entities.Ocupation
 
 class RequestFragment : Fragment() {
 
     lateinit var v : View
+    lateinit var spnOcupation : Spinner
+
+    var ocupationList : List<String> = Ocupation().getList()
+    lateinit var ocupationAdapter: ArrayAdapter<String>
+    lateinit var selectedOcupation : String
+
+
+    lateinit var serviceTypesAdapter: ArrayAdapter<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_request, container, false)
+        spnOcupation = v.findViewById(R.id.spnOcupations)
         return v
     }
 
+    override fun onStart() {
+        super.onStart()
+
+        setSpinners()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        ocupationAdapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, ocupationList)
+    }
+
+    private fun setSpinners() {
+        spnOcupation.adapter = ocupationAdapter
+        spnOcupation.onItemSelectedListener = object:
+            AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    selectedOcupation = ocupationList[position]
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    showDialog()
+                }
+            }
+    }
+
+    private fun showDialog(){
+        val dialog = AlertDialog.Builder(context)
+            .setTitle("Error")
+            .setMessage("ROMPISTE TODO")
+            .setCancelable(true)
+            .create()
+        dialog.show()
+    }
 }
