1d5248f1b62f19b9395315c492ddec9b3a280070
Pedro <pkupersmit@gmail.com>
2023-09-18 12:03:20 -0300

    Desing/recycler view xml (#3)
    * wip: recycler for request list
    
    * feat: :sparkles: add RecyclerView for requests

---
+++ app/src/main/java/com/ar/of_pro/adapters/RequestCardAdapter.kt
@@ -0,0 +1,29 @@
+package com.ar.of_pro.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.ar.of_pro.R
+import com.ar.of_pro.entities.Request
+import com.ar.of_pro.holders.RequestCardHolder
+
+class RequestCardAdapter(private val requestList: MutableList<Request>) :
+    RecyclerView.Adapter<RequestCardHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RequestCardHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.request_card_element, parent, false)
+        return (RequestCardHolder(view))
+    }
+
+    override fun getItemCount(): Int {
+        return requestList.size
+    }
+
+    override fun onBindViewHolder(holder: RequestCardHolder, position: Int) {
+        val request = requestList[position]
+        holder.setTitle(request.requestTitle)
+        holder.setBidsAmount(request.requestBidsAmount)
+        //TODO setup listener and function to the onClick event
+    }
+
+}
--- app/src/main/java/com/ar/of_pro/fragments/RequestsListFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/RequestsListFragment.kt
@@ -5,19 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ar.of_pro.R
+import com.ar.of_pro.adapters.RequestCardAdapter
+import com.ar.of_pro.entities.Request
 
 class RequestsListFragment : Fragment() {
 
-    lateinit var v : View
-
+    lateinit var v: View
+    lateinit var recRequestList: RecyclerView
+    var requestList: MutableList<Request> = ArrayList()
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    private lateinit var requestListAdapter: RequestCardAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_requests_list, container, false)
+        recRequestList = v.findViewById(R.id.rec_requestsList)
         return v
     }
+
+    override fun onStart() {
+        super.onStart()
+       for (i in 1..10) {
+           requestList.add(Request("Pintar paredes en una cocina", 9))
+           requestList.add(Request("Arreglar canilla que pierde", 0))
+           requestList.add(Request("Instalar aire acondicionado", 23))
+       }
+
+        recRequestList.setHasFixedSize(true)
+        linearLayoutManager = LinearLayoutManager(context)
+        recRequestList.layoutManager = linearLayoutManager
+        requestListAdapter = RequestCardAdapter(requestList)
+        recRequestList.adapter = requestListAdapter
+
+    }
+
+
 
 }
