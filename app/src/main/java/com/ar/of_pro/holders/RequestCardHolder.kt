0c8168cb0d29ea04343ac567d2b46445c3bfbee8
Pedro <pkupersmit@gmail.com>
2023-09-18 19:31:07 -0300

    Desing/recycler view xml (#4)
    * wip: recycler for request list
    
    * feat: :sparkles: add RecyclerView for requests
    
    * feat: add navigation between recyclerList and proposal

--- app/src/main/java/com/ar/of_pro/holders/RequestCardHolder.kt
+++ app/src/main/java/com/ar/of_pro/holders/RequestCardHolder.kt
@@ -2,6 +2,9 @@ package com.ar.of_pro.holders
 
 import android.view.View
 import android.widget.TextView
+
+import androidx.cardview.widget.CardView
+
 import androidx.recyclerview.widget.RecyclerView
 import com.ar.of_pro.R
 
@@ -22,6 +25,10 @@ class RequestCardHolder(v: View) : RecyclerView.ViewHolder(v) {
     fun setBidsAmount(bidAmount: Int ) {
         val txt : TextView = view.findViewById(R.id.requestCardBidsAmount)
         txt.text = bidAmount.toString()
+    }
+
+    fun getCardLayout (): CardView {
+        return view.findViewById(R.id.card_package_item)
     }
 }
 
