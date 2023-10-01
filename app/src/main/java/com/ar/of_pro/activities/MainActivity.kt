13d2b602c4795ce95db565308c80fb73fdc3a708
flerner <felix.lerner01@gmail.com>
2023-10-01 19:17:43 -0300

    feat: created a second bottom menu for providers

--- app/src/main/java/com/ar/of_pro/activities/MainActivity.kt
+++ app/src/main/java/com/ar/of_pro/activities/MainActivity.kt
@@ -4,62 +4,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ar.of_pro.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
-import com.ar.of_pro.fragments.HistoryFragment
-import com.ar.of_pro.fragments.RequestFragment
 import com.ar.of_pro.fragments.RequestsListFragment
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var bottomNavigation : BottomNavigationView
+    lateinit var bottomNavigationClient : BottomNavigationView
+    lateinit var bottomNavigationProvider : BottomNavigationView
     lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        bottomNavigation = findViewById(R.id.bottomNavView) as BottomNavigationView
         navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostView) as NavHostFragment
 
-        bottomNavigation = findViewById(R.id.bottomNavView)
+        bottomNavigationClient = findViewById(R.id.bottomNavViewClient) as BottomNavigationView
+        bottomNavigationClient = findViewById(R.id.bottomNavViewClient)
 
-        NavigationUI.setupWithNavController(bottomNavigation, navHostFragment.navController)
-        /*bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.requestsListFragment -> {
-                    loadFragment(RequestsListFragment())
-                    true
-                }
-                R.id.historyFragment -> {
-                    loadFragment(HistoryFragment())
-                    true
-                }
-                R.id.requestFragment -> {
-                    loadFragment(RequestFragment())
-                    true
-                }
+        bottomNavigationProvider = findViewById(R.id.bottomNavViewProvider) as BottomNavigationView
+        bottomNavigationProvider = findViewById(R.id.bottomNavViewProvider)
 
-                else -> {
-                    false
-                }
-            }
-        }*/
+
+
+        //NavigationUI.setupWithNavController(bottomNavigationClient, navHostFragment.navController)
+        NavigationUI.setupWithNavController(bottomNavigationProvider, navHostFragment.navController)
     }
-   /* private fun loadFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragments = fragmentManager.fragments
 
-        if (!fragments.isEmpty()) {
-            val transaction = fragmentManager.beginTransaction()
-            for (currentFragment in fragments) {
-                transaction.remove(currentFragment)
-            }
-            transaction.commitNow()
-        }
 
-        val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.mainLayout, fragment)
-        transaction.commit()
-    }*/
+
 }
---
+++ app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.ar.of_pro.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ar.of_pro.R
+
+
+class ProviderProfileFragment : Fragment() {
+
+    lateinit var v : View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_provider_profile, container, false)
+        return v
+    }
+
+}
