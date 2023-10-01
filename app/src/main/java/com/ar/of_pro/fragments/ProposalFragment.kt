35717b7d32eb6764111bd8d18219d64cdc2b78d2
flerner <felix.lerner01@gmail.com>
2023-10-01 18:40:03 -0300

    feat: completed flow from ProposalFragment to RequestListFragment

--- app/src/main/java/com/ar/of_pro/fragments/ProposalFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/ProposalFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import com.ar.of_pro.R
 
 class ProposalFragment : Fragment() {
 
     lateinit var v : View
+    lateinit var btnProposal : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_proposal, container, false)
+        btnProposal = v.findViewById(R.id.btnProposal)
         return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+        btnProposal.setOnClickListener{
+            val action =
+                ProposalFragmentDirections.actionProposalFragmentToRequestsListFragment()
+            v.findNavController().navigate(action)
+        }
     }
 
 }
