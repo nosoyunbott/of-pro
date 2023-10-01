0bbf05237c79230f9658083b7fc6945beca79eb0
flerner <felix.lerner01@gmail.com>
2023-10-01 19:30:22 -0300

    feat: edit profile button working

--- app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
@@ -5,20 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 import com.ar.of_pro.R
 
 
 class ProviderProfileFragment : Fragment() {
 
     lateinit var v : View
-
+    lateinit var btnEdit : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_provider_profile, container, false)
+        btnEdit = v.findViewById(R.id.btnEdit)
         return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+        btnEdit.setOnClickListener{
+            val action = ProviderProfileFragmentDirections.actionProviderProfileFragmentToProviderProfileEditFragment()
+            v.findNavController().navigate(action)
+        }
     }
 
 }
