1d8f61673242fc4c8741f7fed95fee8cfddb18d7
flerner <felix.lerner01@gmail.com>
2023-10-02 21:00:29 -0300

    AppFragment now decides the flow

--- app/src/main/java/com/ar/of_pro/fragments/AppFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/AppFragment.kt
@@ -1,10 +1,12 @@
 package com.ar.of_pro.fragments
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.ar.of_pro.R
 
 class AppFragment : Fragment() {
@@ -15,6 +17,17 @@ class AppFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_app, container, false)
+
+        Handler().postDelayed({
+            if(false){
+                val action = AppFragmentDirections.actionAppFragmentToRequestsListFragment()
+                v.findNavController().navigate(action)
+            }else{
+                val action = AppFragmentDirections.actionAppFragmentToRequestListProviderFragment()
+                v.findNavController().navigate(action)
+            }
+        },500)
+
         return v
     }
 
