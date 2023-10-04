5c9c7988400f9a199d758eafbe3cac28be19d3e0
felor <felodrobles@gmail.com>
2023-10-03 21:43:17 -0300

    fixed bottom nav view requests list bug

--- app/src/main/java/com/ar/of_pro/activities/MainActivity.kt
+++ app/src/main/java/com/ar/of_pro/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ar.of_pro.activities
 import android.content.ClipData.Item
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import com.ar.of_pro.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -30,8 +31,10 @@ class MainActivity : AppCompatActivity() {
         bottomNavigation = findViewById(R.id.bottomNavView)
         if(userType == "CLIENT"){
             bottomNavigation.menu.findItem(R.id.providerProfileFragment).isVisible = false
+            bottomNavigation.menu.findItem(R.id.requestListProviderFragment).isVisible = false
         }else{
             bottomNavigation.menu.findItem(R.id.requestFragment).isVisible = false
+            bottomNavigation.menu.findItem(R.id.requestsListFragment).isVisible = false
         }
 
 
--- app/src/main/java/com/ar/of_pro/fragments/AppFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/AppFragment.kt
@@ -2,6 +2,7 @@ package com.ar.of_pro.fragments
 
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
