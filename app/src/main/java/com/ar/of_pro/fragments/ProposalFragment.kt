175cbb8b6eaf0ce2025d3543c49384ef31e01977
felor <felodrobles@gmail.com>
2023-10-07 16:58:13 -0300

    proposal fragment recieves request object from card and loads info on fragment

--- app/src/main/java/com/ar/of_pro/fragments/ProposalFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/ProposalFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 import com.ar.of_pro.R
 
@@ -13,6 +14,12 @@ class ProposalFragment : Fragment() {
 
     lateinit var v : View
     lateinit var btnProposal : Button
+    lateinit var txtTitle : TextView
+    lateinit var txtOcupation : TextView
+    lateinit var txtServiceType : TextView
+    lateinit var txtTime : TextView
+    lateinit var txtPricing : TextView
+    lateinit var txtDescription : TextView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -20,11 +27,26 @@ class ProposalFragment : Fragment() {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_proposal, container, false)
         btnProposal = v.findViewById(R.id.btnProposal)
+        txtTitle = v.findViewById(R.id.txtTitle)
+        txtOcupation = v.findViewById(R.id.txtOcupation)
+        txtServiceType = v.findViewById(R.id.txtServiceType)
+        txtTime = v.findViewById(R.id.txtTime)
+        txtPricing = v.findViewById(R.id.txtPricing)
+        txtDescription = v.findViewById(R.id.txtDescription)
         return v
     }
 
     override fun onStart() {
         super.onStart()
+
+        val request = ProposalFragmentArgs.fromBundle(requireArguments()).request
+        txtTitle.text = request.requestTitle
+        txtOcupation.text = request.categoryOcupation
+        txtServiceType.text = request.categoryService
+        txtTime.text = request.date
+        txtPricing.text = request.maxCost.toString()
+        txtDescription.text = request.description
+
         btnProposal.setOnClickListener{
             val action =
                 ProposalFragmentDirections.actionProposalFragmentToRequestListProviderFragment()
