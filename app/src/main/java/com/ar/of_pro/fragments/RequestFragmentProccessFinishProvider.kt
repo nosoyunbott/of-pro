83b5f5cb52974d93a865be5e7b533e27aad3c99e
felor <felodrobles@gmail.com>
2023-10-10 22:39:20 -0300

    TODOs implementados, reqhis_item modificado

--- app/src/main/java/com/ar/of_pro/fragments/RequestFragmentProccessFinishProvider.kt
+++ app/src/main/java/com/ar/of_pro/fragments/RequestFragmentProccessFinishProvider.kt
@@ -30,6 +30,10 @@ class RequestFragmentProccessFinishProvider : Fragment() {
 
         val request = ProposalFragmentArgs.fromBundle(requireArguments()).request
 
+        //TODO traer todos los datos de la request linea 31 y cargarlos en el fragment desde la db
+
 
     }
+
+    //TODO elaborar finalizacion del contrato y pasar estado a PENDING
 }
