5ab82791dcad49aaceaf6de50bd1bc5c6023126f
juanbultri <juanbultri8@gmail.com>
2023-10-12 21:39:48 -0300

    functional profile test

--- app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
@@ -6,14 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.navigation.findNavController
 import com.ar.of_pro.R
+import com.google.firebase.firestore.FirebaseFirestore
+import com.ar.of_pro.R.layout.fragment_provider_profile
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.DocumentSnapshot
 
 
 class ProviderProfileFragment : Fragment() {
 
-    lateinit var v : View
-    lateinit var btnEdit : Button
+    lateinit var v: View
+    lateinit var btnEdit: Button
+
+
+    val db = FirebaseFirestore.getInstance()
+    val userRequest = db.collection("Users")
+    val currentUser = FirebaseAuth.getInstance().currentUser
+    val mail = currentUser?.email
+    lateinit var txtNombre:TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -21,15 +35,38 @@ class ProviderProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_provider_profile, container, false)
         btnEdit = v.findViewById(R.id.btnEdit)
+        txtNombre=v.findViewById(R.id.txtNombre)
+
+
         return v
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        userRequest.get().addOnSuccessListener { snapshots ->
+            for (snapshot in snapshots) {
+
+                if (snapshot.getString("mail") == "asd@123.com") {
+                    txtNombre.text=snapshot.getString("fullname")
+
+                }
+            }
+
+
+        }
+
+
     }
 
     override fun onStart() {
         super.onStart()
-        btnEdit.setOnClickListener{
-            val action = ProviderProfileFragmentDirections.actionProviderProfileFragmentToProviderProfileEditFragment()
+        btnEdit.setOnClickListener {
+            val action =
+                ProviderProfileFragmentDirections.actionProviderProfileFragmentToProviderProfileEditFragment()
             v.findNavController().navigate(action)
         }
     }
+
 
 }
