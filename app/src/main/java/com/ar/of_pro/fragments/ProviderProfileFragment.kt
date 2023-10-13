7e12dfbf4c8111da883aa3a1472bffe01fc2b756
juanbultri <juanbultri8@gmail.com>
2023-10-13 01:09:27 -0300

    profile-with-firestore-data

--- app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/ProviderProfileFragment.kt
@@ -27,6 +27,12 @@ class ProviderProfileFragment : Fragment() {
     val currentUser = FirebaseAuth.getInstance().currentUser
     val mail = currentUser?.email
     lateinit var txtNombre:TextView
+    lateinit var txtLocalidad:TextView
+    lateinit var txtCorreo:TextView
+    lateinit var txtTelefono:TextView
+    lateinit var numRating:TextView
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,7 +42,10 @@ class ProviderProfileFragment : Fragment() {
         v = inflater.inflate(R.layout.fragment_provider_profile, container, false)
         btnEdit = v.findViewById(R.id.btnEdit)
         txtNombre=v.findViewById(R.id.txtNombre)
-
+        txtLocalidad=v.findViewById(R.id.txtLocalidad)
+        txtCorreo =v.findViewById(R.id.txtCorreo)
+        txtTelefono=v.findViewById(R.id.txtTelefono)
+        numRating=v.findViewById(R.id.numRating)
 
         return v
     }
@@ -47,9 +56,18 @@ class ProviderProfileFragment : Fragment() {
         userRequest.get().addOnSuccessListener { snapshots ->
             for (snapshot in snapshots) {
 
-                if (snapshot.getString("mail") == "pepito@pepe.com") {
+                if (snapshot.getString("mail") == "client@test.com") {
                     val fullName = snapshot.getString("fullName") ?: ""
+                    val location = snapshot.getString("location") ?: ""
+                    val mail = snapshot.getString("mail") ?: ""
+                    val phoneNumber = snapshot.getString("phoneNumber")?:""
+                    val numRating = snapshot.hashCode()
                     txtNombre.text = fullName
+                    txtLocalidad.text= location
+                    txtCorreo.text=mail
+                    txtTelefono.text=phoneNumber
+
+
 
                 }
             }
