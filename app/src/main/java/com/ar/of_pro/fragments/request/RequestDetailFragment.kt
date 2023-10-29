348e793e1ed23c5831d71696811410f93daf8583
Felipe Fluck <felipe@fluck.com.ar>
2023-10-28 21:16:31 -0300

    added picture and location to request detail fragment

--- app/src/main/java/com/ar/of_pro/fragments/request/RequestDetailFragment.kt
+++ app/src/main/java/com/ar/of_pro/fragments/request/RequestDetailFragment.kt
@@ -6,11 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.ar.of_pro.R
 import com.ar.of_pro.services.RequestsService
+import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FirebaseFirestore
 
 class RequestDetailFragment : Fragment() {
     lateinit var v: View
@@ -24,6 +28,9 @@ class RequestDetailFragment : Fragment() {
     lateinit var txtLocation: TextView
     lateinit var txtCalification: TextView
     lateinit var txtCalificationQty: TextView
+    lateinit var imgHeader: ImageView
+
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -31,7 +38,7 @@ class RequestDetailFragment : Fragment() {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_request_detail, container, false)
         btnAccept = v.findViewById(R.id.btnAccept)
-        btnDeny= v.findViewById(R.id.btnDeny)
+        btnDeny = v.findViewById(R.id.btnDeny)
         txtDesc = v.findViewById(R.id.txtDesc)
         txtBid = v.findViewById(R.id.txtBid)
         profileHeader = v.findViewById(R.id.proposalProfileHeader)
@@ -39,14 +46,14 @@ class RequestDetailFragment : Fragment() {
         txtLocation = profileHeader.findViewById(R.id.headerLocation)
         txtCalification = profileHeader.findViewById(R.id.headerRating)
         txtCalificationQty = profileHeader.findViewById(R.id.headerRatingQuantity)
+        imgHeader = profileHeader.findViewById(R.id.headerImage)
         return v
     }
 
     @SuppressLint("SetTextI18n")
     override fun onStart() {
         super.onStart()
-        val action =
-            RequestDetailFragmentDirections.actionRequestDetailFragmentToRequestsListFragment()
+
         val proposalInfo = RequestDetailFragmentArgs.fromBundle(
             requireArguments()
         ).proposalInformation
@@ -55,12 +62,29 @@ class RequestDetailFragment : Fragment() {
         txtName.text = proposalInfo.name
         //proposalInfo.calification se esta truncando ¿como mostrar el dato bien?
         txtCalification.text = "${proposalInfo.calification} ${resources.getString(R.string.star)}"
-        txtCalificationQty.text = proposalInfo.calificationQty.toString()
+        txtCalificationQty.text = " (${proposalInfo.calificationQty})"
 
-        btnAccept.setOnClickListener{
-            RequestsService.updateProviderIdFromRequest(proposalInfo.requestId, proposalInfo.providerId)
+        val users = db.collection("Users")
+        users.whereEqualTo(FieldPath.documentId(), proposalInfo.providerId).get()
+            .addOnSuccessListener { querySnapshot ->
+                for (snapshot in querySnapshot) {
 
-            v.findNavController().popBackStack(v.findNavController().graph.startDestinationId, false)
+                    val profileImage = snapshot.getString("imageUrl") ?: ""
+                    Glide.with(requireContext()).load(profileImage).into(imgHeader)
+                    val location = snapshot.getString("location") ?: ""
+                    txtLocation.text = location
+                }
+
+            }
+
+        btnAccept.setOnClickListener {
+            RequestsService.updateProviderIdFromRequest(
+                proposalInfo.requestId,
+                proposalInfo.providerId
+            )
+
+            v.findNavController()
+                .popBackStack(v.findNavController().graph.startDestinationId, false)
             //v.findNavController().navigate(action)
         }
     }
