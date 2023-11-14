97579adcc155f23a6d9f379ec16f50dec34623bb
Pedro <pkupersmit@gmail.com>
2023-11-13 21:22:25 -0300

    feat: disable darkmode (#56)

--- app/src/main/java/com/ar/of_pro/activities/AuthActivity.kt
+++ app/src/main/java/com/ar/of_pro/activities/AuthActivity.kt
@@ -1,12 +1,14 @@
 package com.ar.of_pro.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.ar.of_pro.R
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
     }
